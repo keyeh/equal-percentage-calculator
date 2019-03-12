fde91426d197f9e4e43a85cd9bca9afb3dde2460
keyeh <keyeh@users.noreply.github.com>
2019-03-11 18:47:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      personOneIncome: 145000,
+      personTwoIncome: 120000,
+      inputAmount: 3500
+    }
+
+    this.calculateAnswer = this.calculateAnswer.bind(this);
+  }
+
+  calculateAnswer() {
+    const {personOneIncome, personTwoIncome, inputAmount} = this.state;
+    const percent = inputAmount / (personOneIncome + personTwoIncome);
+
+    return {
+      personOneAmount: (personOneIncome * percent).toFixed(2),
+      personTwoAmount: (personTwoIncome * percent).toFixed(2)
+    }
+  }
+
   render() {
+    const {personOneAmount, personTwoAmount} = this.calculateAnswer()
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Person #1 earns: $
+        <input type="number" onChange={(e) => this.setState({personOneIncome: Number(e.target.value)})} value={this.state.personOneIncome}/>
+        <br/>
+        Person #2 earns: $
+        <input type="number" onChange={(e) => this.setState({personTwoIncome: Number(e.target.value)})} value={this.state.personTwoIncome}/>
+        <br/>
+        Amount to split: $
+        <input type="number" onChange={(e) => this.setState({inputAmount: Number(e.target.value)})} value={this.state.inputAmount}/>
+        <br/>
+        <h3>Person 1 pays: ${personOneAmount}</h3>
+        <h3>Person 2 pays: ${personTwoAmount}</h3>
       </div>
     );
   }
